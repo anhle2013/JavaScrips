19ef84c575dce2df5a9adf17ab2f8446fd7e48e5
Your Name <anhlee.2013@gmail.com>
2022-03-08 15:15:41 +0700

    aaa

---
+++ demo_file_JavaScrips.js
@@ -0,0 +1,13 @@
+//alert("Đã tạo trang web"); //hiện hộp thoại thông báo
+//var b = prompt("Nhập giá trị a="); //nhập vào giá trị
+//console.log("Giá trị b là: ", b); // xem giá trị, chuột phải --> Inspect --> Console
+//console.log("Thông báo abc");
+//console.log("Giá trị là ", 1)
+var x,y,z;
+//x=5; y=6;
+x = prompt("Nhập giá trị x");
+y = prompt("Nhập giá trị y");
+//z=x+y; // cho giá trị chuỗi
+z= parseInt(x) + parseInt(y); // chuyển về số nguyên
+//alert("Giá trị z=" + z); // dùng dấu "+" k dùng ","
+console.log("Giá trị z= " + z) // dùng dấu "+" thì ra chuỗi, dấu "," ra giá trị
